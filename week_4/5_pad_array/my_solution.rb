11019ef53948abab541993dbffe2fa885c0ce642
scyclow <spike716@gmail.com>
2014-06-06 10:53:23 -0500

    pseudocoded

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -8,9 +8,16 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# for the Array class, 
+	# define a method called pad, accepting two paramaters: times and with. with is set to nil on default
+		# create a variable called new array, equal to self
+		# if times is negative or 0, return the array
+		# 'times'.times, shove 'with' into the back of the array	
+		# return new array
+	# define a method called pad!, accepting two paramaters: times and with. with is set to nil on default
+		# if times is negative or 0, return self
+		# 'times'.times, shove 'with' into the back of self
+		# return self
 
 
 # 2. Initial Solution
