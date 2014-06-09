c3269c37b1dec53328d78d1199112f236d140dce
scyclow <spike716@gmail.com>
2014-06-09 17:11:40 -0500

    tweaked group prooj

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -6,46 +6,70 @@ my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4,
 
 # Person 1's solution
 def my_array_finding_method(source, thing_to_find)
-  # Your code here!
+	source.select{|i| i.to_s.include? thing_to_find}
 end
 
 def my_hash_finding_method(source, thing_to_find)
-  # Your code here!
+    source.select{|k, v| v == thing_to_find}.keys
 end
 
 # Identify and describe the ruby method you implemented. 
-# 
-#
-#
+# I implemented the each method. First I thought I was going to need an
+# include method, but then I realized to output these values, I would need 
+# to first iterate over them. The each method works as an iterator. 
 
 # Person 2
 def my_array_modification_method(source, thing_to_modify)
-  # Your code here!
+    source.each_with_index do |value, index|
+        if value.is_a? Integer
+           source[index] += thing_to_modify
+        end
+    end
 end
 
+
 def my_hash_modification_method(source, thing_to_modify)
-  # Your code here!
+    source.each_with_index do |(key, value), index|
+        source[key] = value + thing_to_modify
+	end
 end
 
 # Identify and describe the ruby method you implemented. 
-# 
-#
-#
+
+# my_array_modification_method
+# .map!: takes two arguements, the element and the block. It modifies the array changes each element of 
+# 	     the array with the value returned by the block
+# .is_a?: checks for the class of the object in question. In this case I'm checking whether an element 
+# 		  within the array is an integer. If so, it will increase by the value entered in thing_to_modify
+# 		  If the element is not an integer, the block returns the original element back into the array. 
+
+# my_hash_modification_method 
+# .each: also takes in two arguements representing each key and value of the hash. 
+#	     source[k] calls each of the key in the hash and modifies their values by what is entered for 
+#		 thing_to_modify
 
 
 # Person 3
 def my_array_sorting_method(source)
-  # Your code here!
+	source.collect{|x| x.to_s}.sort
+	puts source
 end
 
 def my_hash_sorting_method(source)
-  # Your code here!
+	source.sort_by{|k, v| v }
+	puts source
 end
 
 # Identify and describe the ruby method you implemented. 
-# 
-#
-#
+
+# For my array sorting method, I used three ruby methods:
+# .collect: .collect returns a new array. Typically, you follow .collect with a block, and the array it returns is the original input array with the effects of this block applied to it.
+# .to_s: I used .to_s as the block for collect. This converted every element of the array to a string and let me sort them.
+# .sort: .sort sorts every item in an array alphabetically. It only works on strings, though, which is why the original array had to be changed.
+
+# .sort_by: This method lets you pick what aspect of the hash you want to sort by. You simply give the method a block with the directions for what you want to sort the hash by and then run it.
+
+# For both of these, I was writing them before the modifications had been made, so I got false results, but when I ran them in irb, I was able to visually match the output to what was required.
 
 
 # Person 4
