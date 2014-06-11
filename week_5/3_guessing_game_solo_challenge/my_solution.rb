708a4a75603853f43bf900c841f3de118d6d8424
scyclow <spike716@gmail.com>
2014-06-11 18:53:05 -0500

    did solo

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -8,33 +8,76 @@
 # Input:
 # Output:
 # Steps:
+# initialize guessing game class with one parameter: answer
+	# set attr_reader for solved?
+	# set answer equal to instance variable
+	# create an instnace variable solved?, and set it equal to false
+#create a method called guess, taking a guess param
+	#if guess equals answer, return :correct and set solved? = true
+	# elsif higher than answer, return :high, 
+	# else return :low
 
 
 # 3. Initial Solution
-
+<<-INITIAL
 class GuessingGame
   def initialize(answer)
-    # Your initialization code goes here
+    @answer = answer
+    @solved = false
   end
-  
-  # Make sure you define the other required methods, too
-end
 
+  def guess(g)
+  	if g == @answer
+  		@solved = true; :correct
+  	elsif g > @answer
+  		:high
+  	else
+  		:low
+  	end
+  end
+
+  def solved?
+  	@solved
+  end
+end
+INITIAL
 
 
 
 # 4. Refactored Solution
 
+class GuessingGame
+  def initialize(answer)
+    @answer = answer
+    @solved = false
+  end
 
+  def guess(g)
+  	if g == @answer then @solved = true; :correct
+  	elsif g > @answer then :high
+  	else :low end
+  end
+  
+  def solved?
+  	@solved
+  end
+end
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
+game = GuessingGame.new(10)
+game.solved? == false
+game.guess(100) == :high
+game.guess(0) == :low
+game.guess(10) == :correct
+game.solved? == true
 
 
 
 
 # 5. Reflection 
+# In this exercise, I learned that variables are not allowed to have a ? in them. 
+# I also learned that, when trying to fit an else on one line, you don't use 'then'.
