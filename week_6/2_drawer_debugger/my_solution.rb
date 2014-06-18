af8feee6bc0f2d6c7ecf88bfe65e6f0668bf249c
scyclow <spike716@gmail.com>
2014-06-18 12:48:48 -0500

    tested, refactored drawer code

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -8,55 +8,75 @@
 
 class Drawer
 
-attr_reader :contents
+attr_reader :contents, :type, :opened
 
-# Are there any more methods needed in this class?
+	# Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
+	def initialize
+		@contents = []
+		@opened = true
+		@type = "drawer"
+	end
+
+	def open
+		@opened = true
+		puts "You opened the drawer"
+	end
+
+	def close
+		@opened = false
+		puts "You closed the drawer"
+	end 
+
+	def add_item *items
+		items.each do |item|
+			@contents << item
+			puts "You added a #{item.type}"
+		end
+	end
+
+	def remove_item(item = @contents.pop) #what is `#pop` doing?
+		if @contents.include? item
+			@contents.delete(item)
+			puts "You removed #{item.type}"
+			item
+		else
+			Silverware.new("hands")
+		end
+		
+	end
+
+	def dump  # what should this method return?
+		@contents = []
+		puts "Your dumped the drawer; it is now empty."
+	end
+
+	def view_contents
+		puts "The drawer contains:"
+		@contents.each {|silverware| puts "- " + silverware.type }
+	end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+	attr_reader :type, :clean
 
-# Are there any more methods needed in this class?
+	# Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
+	def initialize(type, clean = true)
+		@type = type
+		@clean = clean
+	end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
+	def eat
+		puts "eating with the #{type}"
+		@clean = false
+		puts "The #{type} is now dirty."
+	end
+
+	def clean_silverware
+		@clean = true
+		puts "The #{type} is now clean"
+	end
 
 end
 
@@ -85,18 +105,79 @@ silverware_drawer.view_contents
 silverware_drawer.dump
 silverware_drawer.view_contents #What should this return?
 
-
 fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
 fork.eat
 
 #BONUS SECTION
-# puts fork.clean
-
+puts fork.clean
+puts '$'*40
 # DRIVER TESTS GO BELOW THIS LINE
+def display message, test
+	puts message
+	puts test
+	puts '^'*40
+end
+
+def assert_class thing, expected
+	display "#{thing.type} is in the #{expected} class", (thing.class == expected)
+end
+
+def assert_clean utensil, expected
+	modifier = (expected == false) ? "not " : ""
+
+	display "#{utensil.type} is #{modifier}clean?", utensil.clean == expected
+end
+
+def assert_in_drawer thing, *utensils
+	utensils.each {|ut| display "#{ut.type} is in the #{thing.type}", (thing.contents.include? ut)}
+end
+
+def assert_remove thing, utensil=nil
+	thing.remove_item(utensil)
+	display "#{thing.type} no longer displays #{utensil.type}", !(thing.contents.include? utensil)
+end
+
+def assert_dump thing
+	thing.dump
+	display "There's nothing left in the drawer.", thing.contents.empty?
+end
+
+def assert_open_closed thing, expected
+	expected ? thing.open : thing.close
+	modifier = (expected == false) ? "not " : ""
+	display "#{thing.type} is #{modifier}open.", (thing.opened == expected)
+end
 
 
 
+drawer = Drawer.new
+knife = Silverware.new('knife')
+fork = Silverware.new('fork')
+spoon = Silverware.new('spoon')
+
+assert_class drawer, Drawer
+assert_class knife, Silverware 
+assert_class fork, Silverware 
+assert_class spoon, Silverware 
+
+drawer.add_item knife, spoon, fork
+
+assert_in_drawer drawer, knife, spoon, fork
+
+drawer.contents.each {|utensil| assert_clean utensil, true}
+drawer.contents.each {|utensil| utensil.eat}
+drawer.contents.each {|utensil| assert_clean utensil, false}
+fork.clean_silverware
+assert_clean fork, true
+
+assert_remove drawer, fork
+assert_dump drawer
+assert_open_closed drawer, true
+assert_open_closed drawer, false
 
 
 
 # 5. Reflection 
+# I'm a little confused about the directions here. The reason I'm getting the last error message (when fork is defined the second time in the driver code) is because it's asking to remove an item that is not in the drawer. Hence, fork is set to nil insted of a Silverware object.
+# My work-around is to just create a new silverware object called "hands". 
+# Testing some of the object functionality was also kind of tedious. But, I guess it gave me some good practice writing tests for different methods.
